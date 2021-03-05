dd9f46d3a15f18c16f8a3b510273a3676cf1263c
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-05 15:53:18 +0900

    Remove trash files after installation and deletion

--- 0.setAWS.sh
+++ 0.setAWS.sh
@@ -1,5 +1,5 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
+rm -f unzip awscliv2.zip
 sudo ./aws/install
 aws configure
-
--- 6.deleteCapi.sh
+++ 6.deleteCapi.sh
@@ -1,2 +1,10 @@
 kubectl delete -f yaml/_install/1.cluster-api-components-${CAPI_VERSION}.yaml
 kubectl delete -f yaml/_install/2.infrastructure-components-aws-${AWS_VERSION}.yaml
+
+rm -rf img/ yaml/ aws/ \
+    /usr/local/bin/clusterctl \
+    /usr/local/bin/clusterawsadm \
+    /usr/local/aws-cli \
+    /usr/local/bin/aws \
+    /usr/local/bin/aws_completer
+    
