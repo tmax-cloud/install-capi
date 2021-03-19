706799a1c276524039bef4eaffa3a8e9bb3a7499
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-19 11:38:42 +0900

    fix error

--- manifest/2.1.initON.sh
+++ manifest/2.1.initON.sh
@@ -5,8 +5,8 @@ chmod +x clusterctl
 mv clusterctl /usr/local/bin/clusterctl
 
 ## cert-manager
-curl -L https://github.com/jetstack/cert-manager/releases/download/"$CERT_MANAGER_VERSION"/cert-manager.yaml -o cert-manager-${CERT_MANAGER_VERSION}.yaml > yaml/_template/cert-manager-${CERT_MANAGER_VERSION}.yaml
-cp yaml/_template/cert-manager-${CERT_MANAGER_VESION}.yaml yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
+curl -L https://github.com/jetstack/cert-manager/releases/download/"$CERT_MANAGER_VERSION"/cert-manager.yaml > yaml/_template/cert-manager-${CERT_MANAGER_VERSION}.yaml
+cp yaml/_template/cert-manager-${CERT_MANAGER_VERSION}.yaml yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
 kubectl apply -f yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
 
 ## CAPI
