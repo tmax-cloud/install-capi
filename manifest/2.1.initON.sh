6e897886bd3a89392970aac03697db5bab898d45
CK13 Soohwan Kim <soowh91@gmail.com>
2021-03-19 16:14:17 +0900

    add cert-manager webhook wait

--- manifest/2.1.initON.sh
+++ manifest/2.1.initON.sh
@@ -9,6 +9,9 @@ curl -L https://github.com/jetstack/cert-manager/releases/download/"$CERT_MANAGE
 cp yaml/_template/cert-manager-${CERT_MANAGER_VERSION}.yaml yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
 kubectl apply -f yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
 
+echo "waiting cert-manager-webhook for 20s..."
+sleep 20s
+
 ## CAPI
 curl -L http://github.com/kubernetes-sigs/cluster-api/releases/download/"$CAPI_VERSION"/cluster-api-components.yaml > yaml/_template/cluster-api-components-template-${CAPI_VERSION}.yaml 
 cp yaml/_template/cluster-api-components-template-${CAPI_VERSION}.yaml yaml/_install/1.cluster-api-components-${CAPI_VERSION}.yaml
