4f59cda3261a8a5464136be440465f7a05a3ab93
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-16 15:16:19 +0900

    fix error

--- manifest/2.1.2.install-aws-cn.sh
+++ manifest/2.1.2.install-aws-cn.sh
@@ -4,8 +4,8 @@ sudo cp bin/clusterawsadm /usr/local/bin/clusterawsadm
 clusterawsadm bootstrap iam create-cloudformation-stack
 export AWS_B64ENCODED_CREDENTIALS=$(clusterawsadm bootstrap credentials encode-as-profile)
 
-sed -i 's/gcr.io\/kubebuilder\/kube-rbac-proxy':${KUBE_RBAC_PROXY_VERSION}'/'${REGISTRY}'\/kubebuilder\/kube-rbac-proxy':${KUBE_RBAC_PROXY_VERSION}'/g' yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml
-sed -i 's/us.gcr.io\/k8s-artifacts-prod\/cluster-api-aws\/cluster-api-aws-controller:'${AWS_VERSION}'/'${REGISTRY}'\/k8s-artifacts-prod\/cluster-api-aws\/cluster-api-aws-controller:':${AWS_VERSION}'/g' yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml
+sed -i 's/gcr.io\/kubebuilder\/kube-rbac-proxy:'${KUBE_RBAC_PROXY_VERSION}'/'${REGISTRY}'\/kubebuilder\/kube-rbac-proxy:'${KUBE_RBAC_PROXY_VERSION}'/g' yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml
+sed -i 's/us.gcr.io\/k8s-artifacts-prod\/cluster-api-aws\/cluster-api-aws-controller:'${AWS_VERSION}'/'${REGISTRY}'\/k8s-artifacts-prod\/cluster-api-aws\/cluster-api-aws-controller:'${AWS_VERSION}'/g' yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml
 
 ## Initialize CAPI-provider-aws settings
 cp yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml
