2948ff3c1aeb1729f213509164d8d1a6df600483
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-23 14:21:43 +0900

    fix deletion error

--- manifest/99.delete.sh
+++ manifest/99.delete.sh
@@ -1,5 +1,4 @@
 ## Delete CAPI
 kubectl delete -f yaml/_install/1.cluster-api-components-${CAPI_VERSION}.yaml
-kubectl delete -f yaml/_install/0.cert-manager-${CERT_MANAGER_VERSION}.yaml
 sudo rm -rf yaml/ img/ \
     /usr/local/bin/clusterctl
