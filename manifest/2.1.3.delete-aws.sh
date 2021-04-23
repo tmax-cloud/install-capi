2e5352f5f3c7d1b4ac7b9e9db588ed87c278c1bc
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-23 14:28:43 +0900

    fix deletion error

--- manifest/2.1.3.delete-aws.sh
+++ manifest/2.1.3.delete-aws.sh
@@ -3,7 +3,6 @@ kubectl delete -f yaml/_catalog/1.service-catalog-template-CAPI-aws.yaml
 kubectl delete -f yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml
 clusterawsadm bootstrap iam delete-cloudformation-stack
 sudo rm -f /usr/local/bin/clusterawsadm \
-    yaml/_catalog/1.service-catalog-template-CAPI-aws.yaml
-    yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml \
-    yaml/_template/cluster-aws-template-${AWS_VERSION}.yaml \
-    yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml
+    yaml/_catalog/1.service-catalog-template-CAPI-aws.yaml \
+    yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml \
+    yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml    
--- manifest/99.delete.sh
+++ manifest/99.delete.sh
@@ -1,4 +1,4 @@
 ## Delete CAPI
 kubectl delete -f yaml/_install/1.cluster-api-components-${CAPI_VERSION}.yaml
-sudo rm -rf yaml/ img/ \
+sudo rm -rf yaml/ img/ bin/ \
     /usr/local/bin/clusterctl
