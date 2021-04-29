eff54378534ac765cbc105e3094528c8c4fdf6f9
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-29 14:40:42 +0900

    break main branch

--- manifest/2.1.3.delete-aws.sh
+++
@@ -1,8 +0,0 @@
-## Delete CAPI-provider-aws infrastructure
-kubectl delete -f yaml/_catalog/1.service-catalog-template-CAPI-aws.yaml
-kubectl delete -f yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml
-clusterawsadm bootstrap iam delete-cloudformation-stack
-sudo rm -f /usr/local/bin/clusterawsadm \
-    yaml/_catalog/1.service-catalog-template-CAPI-aws.yaml \
-    yaml/_install/2.1.infrastructure-components-aws-${AWS_VERSION}.yaml \
-    yaml/_template/infrastructure-components-aws-template-${AWS_VERSION}.yaml    
--- manifest/99.delete.sh
+++
@@ -1,4 +0,0 @@
-## Delete CAPI
-kubectl delete -f yaml/_install/1.cluster-api-components-${CAPI_VERSION}.yaml
-sudo rm -rf yaml/ img/ bin/ \
-    /usr/local/bin/clusterctl
