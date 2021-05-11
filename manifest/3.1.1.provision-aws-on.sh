34d3aa5e702724503f28dd22126cd259404051be
ck13 SoohwanKim <soowh91@gmail.com>
2021-05-11 16:59:24 +0900

    fix error

--- manifest/3.1.1.provision-aws-on.sh
+++ manifest/3.1.1.provision-aws-on.sh
@@ -3,13 +3,13 @@ curl -L https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/dow
 
 ## Initialize cluster setting
 cp yaml/_template/cluster-template-aws-${AWS_VERSION}.yaml yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${CLUSTER_NAME}/'"${CLUSTER_NAME}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${AWS_CONTROL_PLANE_MACHINE_TYPE}/'"${AWS_CONTROL_PLANE_MACHINE_TYPE}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${AWS_NODE_MACHINE_TYPE}/'"${AWS_NODE_MACHINE_TYPE}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${AWS_SSH_KEY_NAME}/'"${AWS_SSH_KEY_NAME}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${KUBERNETES_VERSION}/'"${KUBERNETES_VERSION}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${CONTROL_PLANE_MACHINE_COUNT}/'"${CONTROL_PLANE_MACHINE_COUNT}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
-sed -i 's/${WORKER_MACHINE_COUNT}/'"${WORKER_MACHINE_COUNT}"'/g' yaml/_template/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${CLUSTER_NAME}/'"${CLUSTER_NAME}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${AWS_CONTROL_PLANE_MACHINE_TYPE}/'"${AWS_CONTROL_PLANE_MACHINE_TYPE}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${AWS_NODE_MACHINE_TYPE}/'"${AWS_NODE_MACHINE_TYPE}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${AWS_SSH_KEY_NAME}/'"${AWS_SSH_KEY_NAME}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${KUBERNETES_VERSION}/'"${KUBERNETES_VERSION}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${CONTROL_PLANE_MACHINE_COUNT}/'"${CONTROL_PLANE_MACHINE_COUNT}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
+sed -i 's/${WORKER_MACHINE_COUNT}/'"${WORKER_MACHINE_COUNT}"'/g' yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
 
 ## Provision aws cluster
 kubectl apply -f yaml/_install/2.2.cluster-template-aws-${AWS_VERSION}.yaml
