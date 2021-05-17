19c6e32a7dcd4f4f409ec20bc20ed861b08b83a5
ck13 SoohwanKim <soowh91@gmail.com>
2021-05-17 11:45:23 +0900

    remove clusterctl binary

--- manifest/1.1.install-capi.sh
+++ manifest/1.1.install-capi.sh
@@ -1,15 +1,7 @@
 source version.conf
 
-## Install clusterctl
-chmod +x bin/clusterctl
-sudo cp bin/clusterctl /usr/local/bin/clusterctl
-
 ## Download and provision CAPI
 kubectl apply -f yaml/cluster-api-components-${CAPI_VERSION}.yaml
-
-## Check install status
-echo ""
-clusterctl version
 
 echo ""
 echo ""
