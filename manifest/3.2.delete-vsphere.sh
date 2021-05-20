51cb3757fb0d3956e400e0aa516300c94e2300cc
CK13 Soohwan Kim <soowh91@gmail.com>
2021-05-20 11:47:13 +0900

    fix error

--- manifest/3.2.delete-vsphere.sh
+++ manifest/3.2.delete-vsphere.sh
@@ -1,7 +1,7 @@
 source version.conf
 
 ## Delete CAPI-provider-vsphere infrastructure
-kubectl delete -f yaml/service-catalog-template-CAPI-vsphere.yaml
+kubectl delete -f yaml/service-catalog-template-CAPI-vsphere-${VSPHERE_VERSION}.yaml
 kubectl delete -f yaml/infrastructure-components-vsphere-${VSPHERE_VERSION}.yaml
 
 echo ""
