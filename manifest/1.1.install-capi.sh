d273087db18c8bafb48bbb1e3c40a2a997015a97
soohwan kim <soowh91@gmail.com>
2021-07-06 12:35:03 +0900

    fix error for sed

--- manifest/1.1.install-capi.sh
+++ manifest/1.1.install-capi.sh
@@ -3,18 +3,18 @@ if [ ! -f "oidc.conf" ]; then
     exit 0
 fi
 
-cp oidc.conf oidc-for-sed.conf
-sed -i 's/\//\\\\\//g' oidc-for-sed.conf
 source version.conf
-source oidc-for-sed.conf
+source oidc.conf
 
 ## Set oidc configuration to templates
-sed -i 's/${OIDC_ISSUER_URL}/'"${OIDC_ISSUER_URL}"'/g' yaml/service-catalog-template-CAPI-*.yaml
+url_for_sed=$(echo ${OIDC_ISSUER_URL} | sed 's/\//\\\//g')
+file_for_sed=$(echo ${OIDC_CA_FILE} | sed 's/\//\\\//g')
+sed -i 's/${OIDC_ISSUER_URL}/'"${url_for_sed}"'/g' yaml/service-catalog-template-CAPI-*.yaml
 sed -i 's/${OIDC_CLIENT_ID}/'"${OIDC_CLIENT_ID}"'/g' yaml/service-catalog-template-CAPI-*.yaml
 sed -i 's/${OIDC_USERNAME_CLAIM}/'"${OIDC_USERNAME_CLAIM}"'/g' yaml/service-catalog-template-CAPI-*.yaml
 sed -i 's/${OIDC_USERNAME_PREFIX}/'"${OIDC_USERNAME_PREFIX}"'/g' yaml/service-catalog-template-CAPI-*.yaml
 sed -i 's/${OIDC_GROUPS_CLAIM}/'"${OIDC_GROUPS_CLAIM}"'/g' yaml/service-catalog-template-CAPI-*.yaml
-sed -i 's/${OIDC_CA_FILE}/'"${OIDC_CA_FILE}"'/g' yaml/service-catalog-template-CAPI-*.yaml
+sed -i 's/${OIDC_CA_FILE}/'"${file_for_sed}"'/g' yaml/service-catalog-template-CAPI-*.yaml
 sed -i -e '/${HYPERAUTH_CERT}/r '"${OIDC_CA_FILE}" -e '/${HYPERAUTH_CERT}/d' yaml/service-catalog-template-CAPI-*.yaml
 #sed -i ${HYPERAUTH_CERT} yaml/service-catalog-template-CAPI-*.yaml
 #sed -i 's/\\n/\'$'\n''/g' yaml/service-catalog-template-CAPI-*.yaml
@@ -22,5 +22,4 @@ sed -i -e '/${HYPERAUTH_CERT}/r '"${OIDC_CA_FILE}" -e '/${HYPERAUTH_CERT}/d' yam
 ## Download and provision CAPI
 kubectl apply -f yaml/cluster-api-components-${CAPI_VERSION}.yaml
 
-rm -f oidc-for-sed.conf
 bash ./message.sh "SUCCESS" "see 'kubectl get pods -A | grep capi'"
