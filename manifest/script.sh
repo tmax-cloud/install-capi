8ecdc7e9dce9aca5e7dd62d2177afc7889c80301
soohwan kim <soowh91@gmail.com>
2021-08-12 15:37:05 +0900

    aws-credential.form variable error fix

---
+++ manifest/script.sh
@@ -0,0 +1,19 @@
+if [ ! -f "aws-credential.conf" ]; then
+    bash message.sh "ERROR" "'aws-credential.conf' is NOT EXIST!"
+    exit 0
+fi
+
+source version.conf
+source aws-credential.conf
+
+cp ./aws-credential.form ./aws-credential.origin
+sed -i 's#${AWS_ACCESS_KEY_ID}#'${AWS_ACCESS_KEY_ID}'#g' ./aws-credential.form
+sed -i 's#${AWS_SECRET_ACCESS_KEY}#'${AWS_SECRET_ACCESS_KEY}'#g' ./aws-credential.form
+sed -i 's#${AWS_REGION}#'${AWS_REGION}'#g' ./aws-credential.form
+credential=$(cat ./aws-credential.form | base64 | tr -d '\n')
+echo $credential
+## Install CAPA infrastructure comopnents
+#sed -i 's#${AWS_B64ENCODED_CREDENTIALS}#'"$credential"'#g' yaml/infrastructure-components-aws-${AWS_VERSION}.yaml
+#kubectl apply -f yaml/infrastructure-components-aws-${AWS_VERSION}.yaml
+
+#bash message.sh "SUCCESS" "see 'kubectl get pods -A | grep capa'"
